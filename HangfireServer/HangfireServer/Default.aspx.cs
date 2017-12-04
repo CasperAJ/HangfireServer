cc59988be9df2604dbb8dd4c3dce3e032d940aa5
CasperAJ <casper.rts.skole@gmail.com>
2017-12-04 13:28:48 +0100

    First working Server

---
+++ HangfireServer/HangfireServer/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Owin;
+using Hangfire;
+using Hangfire.MySql;
+
+namespace HangfireServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseStorage(new MySqlStorage(""));
+
+            app.UseHangfireServer();
+            //using (var server = new BackgroundJobServer())
+            //{
+            //    Console.WriteLine("Hangfire Server started. Press any key to exit...");
+            //    Console.ReadKey();
+            //}
+        }
+    }
+
+
+    public partial class Default : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
